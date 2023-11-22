52c3888ad3b6152c94de18845ebfff2a88258c16
cuckantinou1 <151449836+cuckantinou1@users.noreply.github.com>
2023-11-22 20:00:50 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -15,25 +15,20 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     let userId = socket.handshake.query.id || 'Usuário Desconhecido';
 
-    // Não adicionar o usuário 'Operador' à lista
-    if (userId !== 'Operador') {
+    if (userId !== 'Operador' && !usersOnline.includes(userId)) {
         usersOnline.push(userId);
     }
 
-    // Emitir lista limitada a 3 usuários, excluindo o usuário 'Operador'
     let limitedUsersOnline = usersOnline.filter(user => user !== 'Operador').slice(0, 10);
     io.emit('user list', limitedUsersOnline);
 
     socket.on('disconnect', () => {
         usersOnline = usersOnline.filter(user => user !== userId);
-        // Emitir lista atualizada após a desconexão
         limitedUsersOnline = usersOnline.filter(user => user !== 'Operador').slice(0, 10);
         io.emit('user list', limitedUsersOnline);
     });
 });
 
-
-// Render.com sets the PORT environment variable for your app
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
