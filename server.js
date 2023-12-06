74130a8e35eeda357caf847a8547dd92b43ddbb5
cuckantinou1 <151449836+cuckantinou1@users.noreply.github.com>
2023-12-06 08:46:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,22 +15,19 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     let userId = socket.handshake.query.id || 'Usuário Desconhecido';
 
-    // Adicionar usuário apenas se não for 'Usuário Desconhecido', 'null' ou 'Operador'
-    if (userId !== 'Operador' && userId !== 'Usuário Desconhecido' && userId !== 'null' && !usersOnline.includes(userId)) {
+    if (userId !== 'Operador' && userId !== 'Usuário Desconhecido' && userId !== 'null' && userId !== null && !usersOnline.includes(userId)) {
         usersOnline.push(userId);
     }
 
-    // Atualize a lógica para emitir a lista de usuários
-    let limitedUsersOnline = usersOnline.filter(user => user !== 'Operador' && user !== 'null').slice(0, 10);
+    let limitedUsersOnline = usersOnline.filter(user => user !== 'Operador' && user !== 'null' && user !== null).slice(0, 10);
     io.emit('user list', limitedUsersOnline);
 
     socket.on('disconnect', () => {
         usersOnline = usersOnline.filter(user => user !== userId);
+        
+        usersOnline = usersOnline.filter(user => user !== 'null' && user !== null);
 
-        // Remover usuários 'null' na desconexão
-        usersOnline = usersOnline.filter(user => user !== 'null');
-
-        limitedUsersOnline = usersOnline.filter(user => user !== 'Operador' && user !=='null').slice(0, 10);
+        limitedUsersOnline = usersOnline.filter(user => user !== 'Operador' && user !== 'null' && user !== null).slice(0, 10);
         io.emit('user list', limitedUsersOnline);
     });
 });
